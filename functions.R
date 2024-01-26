fd02a294c363d5b666f88838cd1f212b3c094474
basak12123 <f.gaj16@gmail.com>
2024-01-26 20:38:16 +0100

    funckja łącząca się z bazą danych

--- functions.R
+++ functions.R
@@ -1,2 +1,15 @@
 library("RPostgres")
 
+open.my.connection <- function() {
+  con <- dbConnect(RPostgres::Postgres(),dbname = 'postgres', # nazwa naszej projektowej bazy
+                   host = 'localhost',
+                   port = 5434, # port ten sam co w psql - zwykle 5432
+                   user = 'postgres', # nasza nazwa użytkownika psql
+                   password = '') # i nasze hasło tego użytkownika
+  return (con)
+}
+
+close.my.connection <- function(con) {
+  dbDisconnect(con)
+}
+
