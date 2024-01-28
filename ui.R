7d347784a87277c8dd0b5b76b0adca355ea2773f
JoannaPokora <pokorajo@gmail.com>
2024-01-28 20:33:17 +0100

    added pages

--- ui.R
+++ ui.R
@@ -1,6 +1,7 @@
 library(shiny)
 library(shinyjs)
 library(shinythemes)
+library(shinyTime)
 source(file = 'functions.r')
 
 
@@ -30,7 +31,7 @@ shinyUI(fluidPage(
                            choices = load.lotniska()),
                dataTableOutput('przyloty.tab')),
 
-      tabPanel('loty pasażera',
+      tabPanel('Loty pasażera',
                textInput(inputId = 'id_biletu',
                          label = 'Podaj numer biletu'),
                textInput(inputId = 'telefon.2',
@@ -40,7 +41,7 @@ shinyUI(fluidPage(
                dataTableOutput('bilety.pasazer'),
                dataTableOutput('bilety.pasazer.2')),
 
-      tabPanel('Zaaktualizuj status',
+      tabPanel('Zaktualizuj status',
                selectInput(inputId = 'id_lotu',
                            label = 'Wybierz lot',
                            choices = load.id.lotu()),
@@ -67,7 +68,55 @@ shinyUI(fluidPage(
                          label = 'Telefon'),
                actionButton(inputId = 'pasazer.to.add',
                             label = 'Dodaj'),
-               verbatimTextOutput("info"))
+               verbatimTextOutput("info")),
+
+      tabPanel('Dodaj lot',
+               dateInput(inputId = 'data',
+                         label = 'Data lotu'),
+               timeInput(inputId = 'godzina',
+                         label = 'Godzina lotu',
+                         seconds = FALSE),
+               selectInput(inputId = 'lotnisko',
+                           label = 'Lotnisko',
+                           choices = load.full.lotniska()),
+               selectInput(inputId = 'stanowisko',
+                         label = 'Stanowisko',
+                         choices = 1:6),
+               selectInput(inputId = 'samolot',
+                           label = 'Samolot',
+                           choices = load.samoloty()),
+               selectInput(inputId = 'pilot',
+                           label = 'Pilot',
+                           choices = load.piloci()),
+               selectInput(inputId = 'kierunek',
+                           label = 'Czy odlatuje?',
+                           choices = c(TRUE, FALSE)),
+               actionButton(inputId = 'lot.to.add',
+                            label = 'Dodaj'),
+               verbatimTextOutput("info_3")),
+
+      tabPanel('Dodaj bilet',
+               selectizeInput(inputId = 'telefon_3',
+                         label = 'Numer telefonu pasażera',
+                         choices = load.numery.telefonu()),
+               dateInput(inputId = 'data_2',
+                         label = 'Data lotu'),
+               selectInput(inputId = 'kierunek_2',
+                              label = 'Kierunek',
+                              choices = c('Odlot', 'Przylot')),
+               selectInput(inputId = 'lot',
+                           label = 'Wybierz lot',
+                           choices = NULL),
+               actionButton(inputId = 'bilet.to.add',
+                            label = 'Dodaj'),
+               verbatimTextOutput("info_4")),
+
+      tabPanel('Usuń bilet',
+               textInput(inputI = 'id_biletu',
+                         label = 'Numer biletu'),
+               actionButton(inputId = 'bilet.to.remove',
+                            label = 'Usuń'),
+               verbatimTextOutput("info_5"))
 
     )
   )
